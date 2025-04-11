3ad6d367f5cefb2f7c96203f6a0a4129c7a801c7
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-04-11 17:01:18 +0200

    wip

---
+++ Sources/HTMLAttributeTypes/Allow.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  swift-html
+//
+//  Created by Coen ten Thije Boonkkamp on 05/04/2025.
+//
+
+
+public struct Allow: BooleanAttribute {
+    /// The name of the HTML attribute
+    public static let attribute: String = "allow"
+    
+    public var value: Bool
+  
+    public init(value: Bool) {
+        self.value = value
+    }
+}
