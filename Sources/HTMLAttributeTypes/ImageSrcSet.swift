8f7bb52fb88332ed02754ec5a04a9f440a855b4e
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-04-11 13:15:04 +0200

    wip

--- Sources/HTMLAttributeTypes/ImageSrcSet.swift
+++ Sources/HTMLAttributeTypes/ImageSrcSet.swift
@@ -5,14 +5,29 @@
 //  Created by Coen ten Thije Boonkkamp on 06/04/2025.
 //
 
-
-public struct ImageSrcSet: Attribute, ExpressibleByStringLiteral {
+public struct ImageSrcSet: Attribute {
+    /// The name of the HTML attribute
     public static let attribute: String = "imagesrcset"
     
-    private var value: String
+    /// The URL value
+    package let value: String
     
+    /// Initialize with a URL string
+    public init(_ value: String) {
+        self.value = value
+    }
+}
+
+extension ImageSrcSet: ExpressibleByStringLiteral {
     public init(stringLiteral value: StringLiteralType) {
         self.value = value
     }
-    
 }
+
+extension ImageSrcSet: CustomStringConvertible {
+    /// Returns the string representation of the imagesrcset value
+    public var description: String {
+        return self.value
+    }
+}
+
