c0a220afa78e6d88f3a504a4ed45d9ec83510b18
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-04-11 13:13:30 +0200

    wip

--- Sources/HTMLAttributeTypes/Imagesizes.swift
+++ Sources/HTMLAttributeTypes/Imagesizes.swift
@@ -1,11 +1,29 @@
 //For rel="preload" and as="image" only, the imagesizes attribute has similar syntax and semantics as the sizes attribute that indicates to preload the appropriate resource used by an img element with corresponding values for its srcset and sizes attributes.
 //
 
-public struct ImageSizes: Attribute, ExpressibleByStringLiteral {
+public struct ImageSizes: Attribute {
+    /// The name of the HTML attribute
     public static let attribute: String = "imagesizes"
-    package let value: StringLiteralType
     
+    /// The URL value
+    package let value: String
+    
+    /// Initialize with a URL string
+    public init(_ value: String) {
+        self.value = value
+    }
+}
+
+extension ImageSizes: ExpressibleByStringLiteral {
     public init(stringLiteral value: StringLiteralType) {
         self.value = value
     }
 }
+
+extension ImageSizes: CustomStringConvertible {
+    /// Returns the string representation of the imagesizes value
+    public var description: String {
+        return self.value
+    }
+}
+
